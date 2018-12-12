ec759b16ffb283bb35855cdc2a09ba4a312963c2
lseroka <35587297+lseroka@users.noreply.github.com>
2018-12-12 11:06:44 -0500

    Add files via upload

--- unhide_folders.py
+++ unhide_folders.py
@@ -14,15 +14,17 @@ def unhide_folders(src_path):
 	for dirpath, dirnames, files in os.walk(src_path):
 		for dirname in dirnames:
 			attribute = win32api.GetFileAttributes(dirpath + '\\' + dirname)
-      
-			if attribute & (win32con.FILE_ATTRIBUTE_HIDDEN | win32con.FILE_ATTRIBUTE_SYSTEM):   #if folder is hidden
-				call(["attrib", "-H", "-S", dirpath + '\\' + dirname, "/S", "/D"])
-				unhidden_list.append(dirname)
-				print(str(dirname) + ' is no longer hidden')
-        
-			else:  #if folder is not hidden
-				print("The folder is already unhidden")
-				pass
+			try:
+				if attribute & (win32con.FILE_ATTRIBUTE_SYSTEM | win32con.FILE_ATTRIBUTE_HIDDEN):   #if folder is hidden
+					call(["attrib", "-s", "-h", dirpath + '\\' + dirname])
+					unhidden_list.append(dirname)
+					print(str(dirname) + ' is no longer hidden')
+			
+				else:  #if folder is not hidden
+					print(str(dirname) + " is already unhidden")
+					pass
+			except:
+				print(str(dirname) + " FAILED to unhide")
 		break
 	print('Found ' + str(len(unhidden_list)) + ' hidden folders')
 
