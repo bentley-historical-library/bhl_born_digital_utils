cb40a330ec0c5c6966b067de2b1753702ec8b728
Hyeeyoung Kim <hyeeyoung.kim@gmail.com>
2019-02-04 14:36:40 -0500

    Update unhide_folder.py argparse argument

--- unhide_folder/unhide_folder.py
+++ unhide_folder/unhide_folder.py
@@ -1,32 +1,33 @@
 import argparse
 import os
 from subprocess import call
-import win32api, win32con
 
+import win32api
+import win32con
 
 parser = argparse.ArgumentParser(description='Check missing folders')
-parser.add_argument('--src', required=True, help='Target directory')
+parser.add_argument('-i', '--input', required=True, help='Input directory')
 args = parser.parse_args()
 
-def unhide_folders(src_path):
-	unhidden_list = []
-  
-	for dirpath, dirnames, files in os.walk(src_path):
-		for dirname in dirnames:
-			attribute = win32api.GetFileAttributes(dirpath + '\\' + dirname)
-			try:
-				if attribute & (win32con.FILE_ATTRIBUTE_SYSTEM | win32con.FILE_ATTRIBUTE_HIDDEN):   #if folder is hidden
-					call(["attrib", "-s", "-h", dirpath + '\\' + dirname])
-					unhidden_list.append(dirname)
-					print(str(dirname) + ' is no longer hidden')
-			
-				else:  #if folder is not hidden
-					print(str(dirname) + " is already unhidden")
-					pass
-			except:
-				print(str(dirname) + " FAILED to unhide")
-		break
-	print('Found ' + str(len(unhidden_list)) + ' hidden folders')
+
+def unhide_folder(src_path):
+    unhidden_list = []
+    for dirpath, dirnames, files in os.walk(src_path):
+        for dirname in dirnames:
+            attribute = win32api.GetFileAttributes(dirpath + '\\' + dirname)
+            try:
+                if attribute & (win32con.FILE_ATTRIBUTE_SYSTEM | win32con.FILE_ATTRIBUTE_HIDDEN):  # if folder is hidden
+                    call(["attrib", "-H", "-S", dirpath + '\\' + dirname])
+                    unhidden_list.append(dirname)
+                    print(str(dirname) + ' is no longer hidden')
+
+                else:  # if folder is not hidden
+                    print(str(dirname) + " is already unhidden")
+                    pass
+            except:
+                print(str(dirname) + " FAILED to unhide")
+        break
+    print('Found ' + str(len(unhidden_list)) + ' hidden folders')
 
 
-unhide_folders(args.src)	
+unhide_folder(args.input)
