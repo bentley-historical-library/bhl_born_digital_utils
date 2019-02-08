46cd85ed643f23d2b85cb75a92da0e6c135bdfd5
Dallas <djpillen@umich.edu>
2019-02-08 15:49:14 -0500

    improve iteration speed for check_os_files.py

--- check_os_files/check_os_files.py
+++ check_os_files/check_os_files.py
@@ -2,7 +2,7 @@ import argparse
 import os
 from subprocess import call, DEVNULL
 
-parser = argparse.ArgumentParser(description='Check Thumbs.db and DS_Store files')
+parser = argparse.ArgumentParser(description='Check Thumbs.db,  DS_Store, Desktop DB, and Desktop DF files')
 parser.add_argument('-b', '--thumbsdb_off', action="store_true", default=False,
                     help='Turn off deleting Thumbs.db files')
 parser.add_argument('-e', '--dsstore_off', action="store_true", default=False,
@@ -20,24 +20,25 @@ args = parser.parse_args()
 
 # Function
 # Searching for target files (* Recursive)
-def search_target(src_path, target):
-    target_list.clear()
-    print('Searching for', target, 'files in', src_path)
+def search_targets(src_path, targets):
+    target_list = []
+    print('Searching for', ", ".join(targets), 'files in', src_path)
     for dirpath, dirnames, files in os.walk(src_path):
         for file in files:
-            if file.endswith(target):
+            if any([target for target in targets if file.endswith(target)]):
                 file_path = os.path.join(dirpath, file)
                 target_list.append(file_path)
+    return target_list
 
 
 # Showing search result for target files and confirming user with deleting those files
-def confirm_delete_target(src_path, target):
+def confirm_delete_targets(src_path, targets, target_list):
     if len(target_list) != 0:
-        print('Found', len(target_list), target, 'file(s) in', src_path)
+        print('Found', len(target_list), ", ".join(targets), 'file(s) in', src_path)
         while True:
-            decision_input = input('Do you want to delete all ' + target + ' file(s)? (y/n) >>> ').replace(' ', '').lower()
+            decision_input = input('Do you want to delete all ' + ", ".join(targets) + ' file(s)? (y/n) >>> ').replace(' ', '').lower()
             if decision_input == 'y':
-                delete_target()
+                delete_targets(target_list)
                 print()
                 break
             if decision_input == 'n':
@@ -45,12 +46,12 @@ def confirm_delete_target(src_path, target):
                 break
             print('Please answer in y or n.')
     else:
-        print('Found', len(target_list), target, 'file in', src_path)
+        print('Found', len(target_list), ", ".join(targets), 'file in', src_path)
         print()
 
 
 # Deleting target files (* Recursive)
-def delete_target():
+def delete_targets(target_list):
     for target in target_list:
         print('Deleting', target)
         try:
@@ -63,18 +64,18 @@ def delete_target():
 
 
 # Script
-target_list = []
+file_extensions_to_delete = ["Thumbs.db", ".DS_Store", "Desktop DB", "Desktop DF"]
+args_to_file_extensions = {
+                    "thumbsdb_off": ["Thumbs.db"],
+                    "dsstore_off": [".DS_Store"],
+                    "desktopdbdf_off": ["Desktop DB", "Desktop DF"]
+                    }
 
-if args.thumbsdb_off is False:
-    search_target(args.input, 'Thumbs.db')
-    confirm_delete_target(args.input, 'Thumbs.db')
+for arg, value in vars(args).items():
+    if value is True and arg in args_to_file_extensions.keys():
+        for file_extension in args_to_file_extensions[arg]:
+            file_extensions_to_delete.remove(file_extension)
 
-if args.dsstore_off is False:
-    search_target(args.input, '.DS_Store')
-    confirm_delete_target(args.input, '.DS_Store')
-
-if args.desktopdbdf_off is False:
-    search_target(args.input, 'Desktop DB')
-    confirm_delete_target(args.input, 'Desktop DB')
-    search_target(args.input, 'Desktop DF')
-    confirm_delete_target(args.input, 'Desktop DF')
+if len(file_extensions_to_delete) > 0:
+    target_list = search_targets(args.input, file_extensions_to_delete)
+    confirm_delete_targets(args.input, file_extensions_to_delete, target_list)
