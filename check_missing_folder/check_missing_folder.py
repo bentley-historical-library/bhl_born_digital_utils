88dc147d254590c8d5946ce821cbb107e417c6ef
MaryseLT <35494636+MaryseLT@users.noreply.github.com>
2019-08-20 14:24:39 -0400

    Increased readability of printed results.

--- check_missing_folder/check_missing_folder.py
+++ check_missing_folder/check_missing_folder.py
@@ -21,8 +21,8 @@ def parse_barcodes(src_path):
                 if row['pass_1_successful'] == 'Y' or row['pass_2_successful'] == 'Y':
                     csv_pass_list.append(row['barcode'].rstrip(' '))
 
-    print('Found', len(csv_fail_list) + len(csv_pass_list), 'barcodes,',
-          len(csv_pass_list), 'passed and', len(csv_fail_list), 'failed, barcodes in bhl_inventory.csv file.')
+    print('\nFound', len(csv_fail_list) + len(csv_pass_list), 'barcodes,',
+          len(csv_pass_list), 'passed, and', len(csv_fail_list), 'failed, barcodes in bhl_inventory.csv file')
 
 
 # Parsing barcode directories in src
@@ -34,8 +34,8 @@ def parse_directories(src_path):
             else:
                 directory_pass_list.append(directory.rstrip(' '))
 
-    print('Found', len(directory_fail_list) + len(directory_pass_list), 'folders',
-          len(directory_pass_list), 'passed and', len(directory_fail_list), 'failed, in', src_path + '.')
+    print('\nFound', len(directory_fail_list) + len(directory_pass_list), 'folders,',
+          len(directory_pass_list), 'passed, and', len(directory_fail_list), 'failed in', src_path)
 
 
 # Cross-comparing two lists
@@ -49,10 +49,10 @@ def compare_lists(src_path):
         if barcode not in directory_fail_list:
             missing_list_A.append(barcode)
 
-    print('Found', len(missing_list_A), 'barcodes not in', src_path + '.')
+    print('\nFound', len(missing_list_A), 'barcodes not in', src_path)
 
     for barcode in missing_list_A:
-        print(barcode, "is missing.")
+        print(barcode)
 
     # Pass B, do barcodes from directory_list in csv_list?
     for barcode in directory_pass_list:
@@ -63,10 +63,10 @@ def compare_lists(src_path):
         if barcode not in csv_fail_list:
             missing_list_B.append(barcode)
 
-    print('Found', len(missing_list_B), 'folders not in', 'bhl_inventory.csv.')
+    print('\nFound', len(missing_list_B), 'folders not in', 'bhl_inventory.csv')
 
     for barcode in missing_list_B:
-        print(barcode, 'is missing.')
+        print(barcode)
 
 
 # Script
@@ -77,7 +77,7 @@ directory_fail_list = []
 missing_list_A = []
 missing_list_B = []
 
-print('Checking missing folders in', args.input)
+print('\nChecking missing folders in', args.input)
 
 parse_barcodes(args.input)
 parse_directories(args.input)
