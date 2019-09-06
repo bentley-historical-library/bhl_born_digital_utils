a3b5d3a5256db27f8cac5fe00fa41bfd1531af38
Dallas <djpillen@umich.edu>
2019-09-06 08:45:52 -0400

    update rename_files to mirror archivematica 1.10

--- bhl_born_digital_utils/rename_files.py
+++ bhl_born_digital_utils/rename_files.py
@@ -1,57 +1,81 @@
 import os
-import string
+import re
+import shutil
+from unidecode import unidecode
 
-# Borrowed heavily from https://github.com/artefactual/archivematica/blob/stable/1.9.x/src/MCPClient/lib/clientScripts/sanitize_names.py
-valid = string.ascii_letters + string.digits + r"-_.()#,&[]%$\+{}~! "
-replacement_char = "_"
-
-
-class RenameFailed(Exception):
-    def __init__(self, code):
-        self.code = code
+# Borrowed heavily from https://github.com/artefactual/archivematica/blob/stable/1.10.x/src/MCPClient/lib/clientScripts/sanitize_names.py
+ALLOWED_CHARS = re.compile(r"[^a-zA-Z0-9\-_\.\(\)#,&%\$\{\}~!] ")
+REPLACEMENT_CHAR = "_"
 
 
 def rename_files(src_path):
-    targets = get_targets(src_path)
-    confirm_renaming(targets)
+    dirs_to_rename, files_to_rename = get_targets(src_path)
+    confirm_renaming(dirs_to_rename, files_to_rename)
 
 
 def get_targets(src_path):
+    dirs_to_rename = []
     files_to_rename = []
-    for root, _, filenames in os.walk(src_path):
-        for filename in filenames:
-                if any([c for c in filename if c not in valid]):
-                    filepath = os.path.join(root, filename)
-                    files_to_rename.append(filepath)
-    return files_to_rename
+    for old_path, sanitized_path, is_dir, is_target in recursively_get_targets(src_path):
+        if is_target:
+            if is_dir:
+                dirs_to_rename.append((old_path, sanitized_path))
+            else:
+                files_to_rename.append((old_path, sanitized_path))
+    return dirs_to_rename, files_to_rename
 
 
-def confirm_renaming(targets):
-    print("*** THE FOLLOWING INVALID FILENAMES HAVE BEEN FOUND ***")
-    print("\n".join(targets))
+def recursively_get_targets(src_path):
+    for dir_entry in os.scandir(src_path):
+        is_dir = dir_entry.is_dir()
+
+        start_path = dir_entry.path
+        basename = os.path.basename(start_path)
+        sanitized_name = sanitize_name(basename)
+        sanitized_path = os.path.join(src_path, sanitized_name)
+
+        is_target = sanitized_path != start_path
+
+        yield start_path, sanitized_path, is_dir, is_target
+
+        if is_dir:
+            for result in recursively_get_targets(start_path):
+                yield result
+
+
+def sanitize_name(basename):
+    unicode_name = unidecode(basename)
+    if unicode_name == "":
+        unicode_name = basename
+    return ALLOWED_CHARS.sub(REPLACEMENT_CHAR, unicode_name)
+
+
+def confirm_renaming(dirs_to_rename, files_to_rename):
+    print("*** DIRECTORIES WILL BE RENAMED AS FOLLOWS ***")
+    for old_path, sanitized_path in dirs_to_rename:
+        print("{} --> {}".format(old_path, sanitized_path))
+    print("\n\n")
+    print("*** FILES WILL BE RENAMED AS FOLLOWS ***")
+    for old_path, sanitized_path in files_to_rename:
+        print("{} --> {}".format(old_path, sanitized_path))
+
     confirm_rename = input("Rename files?: ")
     if confirm_rename.lower().strip() in ["y", "yes"]:
-        n = 1
-        for target in targets:
-            filename = os.path.basename(target)
-            dirname = os.path.dirname(target)
-            sanitized_name = sanitize_name(filename)
-            fileTitle, fileExt = os.path.splitext(sanitized_name)
-            sanitized_path = os.path.join(dirname, fileTitle + fileExt)
-            while os.path.exists(sanitized_path):
-                sanitized_path = os.path.join(dirname, fileTitle + "_" + str(n) + fileExt)
-                n += 1
-            exit_status = os.rename(target, sanitized_path)
-            if exit_status:
-                raise RenameFailed(exit_status)
-            print("{} renamed to {}".format(target, sanitized_path))
+        # reverse the lists so that files and directories deeper in the tree get renamed first
+        dirs_to_rename.reverse()
+        files_to_rename.reverse()
+        rename_paths(files_to_rename)
+        rename_paths(dirs_to_rename)
 
 
-def sanitize_name(filename):
-    sanitized_name = ""
-    for c in filename:
-        if c in valid:
-            sanitized_name += c
-        else:
-            sanitized_name += replacement_char
-    return sanitized_name
+def rename_paths(paths):
+    n = 1
+    for old_path, new_path in paths:
+        dirname = os.path.dirname(old_path)
+        basename = os.path.basename(new_path)
+        filename, fileext = os.path.splitext(basename)
+        sanitized_path = os.path.join(dirname, filename + fileext)
+        while os.path.exists(sanitized_path):
+            sanitized_path = os.path.join(dirname, filename + REPLACEMENT_CHAR + str(n) + fileext)
+            n += 1
+        shutil.move(old_path, sanitized_path)
