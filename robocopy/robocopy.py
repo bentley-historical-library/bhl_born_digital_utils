be2a9cb53d7ad51a7a600e2217427e986ea73a25
MaryseLT <35494636+MaryseLT@users.noreply.github.com>
2019-10-10 10:25:24 -0400

    Updated for Windows 10
    Updated the greeting and resources for the Windows 10 system upgrade. Ran a test transfer on RMW 2 without altering the code and it ran without error.

--- robocopy/robocopy.py
+++ robocopy/robocopy.py
@@ -5,15 +5,16 @@ from subprocess import call
 # This is used to robocopy new transfers to the R: or S: drive depending on your computer.
 # Create a new folder in the R: drive with the name as the accession number BEFORE you robocopy.
 
-# Resource: https://www.computerhope.com/robocopy.htm
+# Resources for Windows 10 PowerShell:
+# https://www.computerhope.com/robocopy.htm
+# https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
 
 parser = argparse.ArgumentParser(description='Remove A/V DIPs from the RMLC spreadsheet.')
 parser.add_argument('-i', '--input', required=True, help='Input directory')
 parser.add_argument('-o', '--output', required=True, help='New output directory')
 args = parser.parse_args()
 
-print("Welcome to Robocopy for Windows 7!\n")
-print("Just a reminder, the University of Michigan will switch to Windows 10 by January 2020.\n")
+print("\nWelcome to Robocopy for Windows 10!\n") # Updated the script 2019-10-10
 
 def robocopy_directory(rmw_dir, new_dir):
     accession_num = input("Please enter the accession number >>> ")
