47b88a947d24e84255d023996b86c7ccf1b4be42
Dallas <djpillen@umich.edu>
2020-01-23 13:10:27 -0500

    fix sys.platform call

--- bhl_born_digital_utils/run_brunnhilde.py
+++ bhl_born_digital_utils/run_brunnhilde.py
@@ -5,7 +5,7 @@ from bhl_born_digital_utils.config import get_config_setting
 
 
 def run_brunnhilde(src, accession_number):
-    if "win" in sys.platform():
+    if "win" in sys.platform:
         print("run_brunnhilde cannot be run on Windows")
     else:
         brunnhilde_dir = get_config_setting("brunnhilde")
