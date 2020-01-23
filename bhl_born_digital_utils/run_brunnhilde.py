57a9f4bf3770a2d601451ab9535a3e22a76e3842
Dallas <djpillen@umich.edu>
2020-01-23 12:58:31 -0500

    add brunnhilde util

---
+++ bhl_born_digital_utils/run_brunnhilde.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+
+from bhl_born_digital_utils.config import get_config_setting
+
+
+def run_brunnhilde(src, accession_number):
+    if "win" in sys.platform():
+        print("run_brunnhilde cannot be run on Windows")
+    else:
+        brunnhilde_dir = get_config_setting("brunnhilde")
+        cmd = [
+            "brunnhilde.py", "-nz",
+            src, brunnhilde_dir, accession_number
+        ]
+
+        subprocess.call(cmd)
